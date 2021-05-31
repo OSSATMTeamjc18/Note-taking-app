345495387d93728a4896ea8e23b9abd823a77faa
Brady Taylor <baetay03@gmail.com>
2021-05-30 19:52:49 -0600

    delete and add function

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -60,3 +60,84 @@ var renderActiveNote = function () {
       renderActiveNote();
     });
   };
+
+  var handleNoteDelete = function (event) {
+    event.stopPropagation();
+  
+    var note = $(this)
+      .parent(".list-group-item")
+      .data();
+  
+    if (activeNote.id === note.id) {
+      activeNote = {};
+    }
+  
+    deleteNote(note.id).then(function () {
+      getAndRenderNotes();
+      renderActiveNote();
+    });
+  };
+  
+
+  
+  var handleNoteView = function () {
+    activeNote = $(this).data();
+    renderActiveNote();
+  };
+  
+ 
+
+  var handleNewNoteView = function () {
+    activeNote = {};
+    renderActiveNote();
+  };
+  
+ 
+
+  var handleRenderSaveBtn = function () {
+    if (!$noteTitle.val().trim() || !$noteText.val().trim()) {
+      $saveNoteBtn.hide();
+    } else {
+      $saveNoteBtn.show();
+    }
+  };
+  
+
+
+  var renderNoteList = function (notes) {
+    $noteList.empty();
+  
+    var noteListItems = [];
+  
+    for (var i = 0; i < notes.length; i++) {
+      var note = notes[i];
+  
+      var $li = $("<li class='list-group-item'>").data(note);
+      var $span = $("<span>").text(note.title);
+      var $delBtn = $(
+        "<i class='fas fa-trash-alt float-right text-danger delete-note'>"
+      );
+  
+      $li.append($span, $delBtn);
+      noteListItems.push($li);
+    }
+  
+    $noteList.append(noteListItems);
+  };
+  
+
+  var getAndRenderNotes = function () {
+    return getNotes().then(function (data) {
+      renderNoteList(data);
+    });
+  };
+  
+
+  $saveNoteBtn.on("click", handleNoteSave);
+  $noteList.on("click", ".list-group-item", handleNoteView);
+  $newNoteBtn.on("click", handleNewNoteView);
+  $noteList.on("click", ".delete-note", handleNoteDelete);
+  $noteTitle.on("keyup", handleRenderSaveBtn);
+  $noteText.on("keyup", handleRenderSaveBtn);
+  
+  getAndRenderNotes();
